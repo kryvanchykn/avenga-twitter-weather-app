b12687737db4861efbc52e18e88a0a456e89819b
nataliia.kryvanchyk <kryvanchyk.n@gmail.com>
2022-06-02 15:09:56 +0300

    Added region inserts to database and methods to change the form of region names. Created class with region constants.

---
+++ src/main/java/com/example/avengatwitterweatherapp/constants/RegionConstants.java
@@ -0,0 +1,8 @@
+package com.example.avengatwitterweatherapp.constants;
+
+public class RegionConstants {
+    public static final int REGION_NAME_SUBS_NUMBER = 9;
+    public static final String REGION_NAME_NEW_END = "ій";
+    public static final int REGION_ALT_NAME_SUBS_NUMBER = 1;
+    public static final String REGION_ALT_NAME_NEW_END = "і";
+}
